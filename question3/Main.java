809cc8df6782c58ddf7ae7afc8752553851149c5
RiadOliveira <riad.oliveira@hotmail.com>
2023-03-10 08:30:31 -0300

    Question 3 fixed

--- question3/Main.java
+++ question3/Main.java
@@ -1,10 +1,8 @@
 package question3;
 
-import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
-  private static Random rand = new Random();
   private static Scanner sc = new Scanner(System.in);
 
   public static boolean isArraySorted(Integer[] array) {
@@ -14,13 +12,6 @@ public class Main {
       }
     }
     return true;
-  }
-
-  public static void printArray(Integer[] array) {
-    for (int i = 0; i < array.length; i++) {
-      System.out.print(array[i] + " ");
-    }
-    System.out.println();
   }
 
   public static void switchNumbers(Integer[] array, int index, int nextIndex) {
@@ -38,50 +29,26 @@ public class Main {
     return -1;
   }
 
-  public static int findFirstUnsortedPairAtOddIndex(Integer[] array) {
-    for (int i = 1; i < array.length; i += 2) {
-      if (array[i] < array[i - 1]) {
-        return i - 1;
-      }
-    }
-    return -1;
-  }
-
   public static void main(String[] args) {
-    int permutationSize;
-    int moves = 0;
     boolean marceloTurn = true;
 
-    System.out.println("Qual o tamanho do arranjo que você deseja? ");
-    permutationSize = sc.nextInt();
+    String values[] = sc.nextLine().split(" ");
+    Integer[] sequence = new Integer[values.length];
 
-    Integer[] sequence = new Integer[permutationSize];
-
-    for (int i = 0; i < permutationSize; i++) {
-      sequence[i] = rand.nextInt(permutationSize);
+    for (int i = 0; i < values.length; i++) {
+      sequence[i] = Integer.valueOf(values[i]);
     }
 
-    System.out.print("ARRANJO GERADO: ");
-    printArray(sequence);
-    System.out.println();
-
     while (!isArraySorted(sequence)) {
-      if (marceloTurn) {
-        System.out.println("==== TURNO DE MARCELO ====");
-        int index = findFirstUnsortedPair(sequence);
+      int index = findFirstUnsortedPair(sequence);
+
+      if(index != -1) {
         switchNumbers(sequence, index, index + 1);
-        printArray(sequence);
-      } else {
-        System.out.println("==== TURNO DE CARLOS ====");
-        int index = findFirstUnsortedPairAtOddIndex(sequence);
-        switchNumbers(sequence, index, index + 1);
-        printArray(sequence);
+        marceloTurn = !marceloTurn;
       }
-      moves++;
-      marceloTurn = !marceloTurn;
     }
 
     String winner = marceloTurn ? "Carlos" : "Marcelo";
-    System.out.println(winner + " ganhou depois de " + moves + " movimentos.");
+    System.out.println(winner);
   }
 }
