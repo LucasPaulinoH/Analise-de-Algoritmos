7cbd615d0ea058fde7a68460784868372029961b
RiadOliveira <riad.oliveira@hotmail.com>
2023-03-10 11:04:58 -0300

    Question 3 correction

--- question3/Main.java
+++ question3/Main.java
@@ -3,52 +3,48 @@ package question3;
 import java.util.Scanner;
 
 public class Main {
-  private static Scanner sc = new Scanner(System.in);
+  private static Scanner scanner = new Scanner(System.in);
 
-  public static boolean isArraySorted(Integer[] array) {
-    for (int i = 0; i < array.length - 1; i++) {
-      if (array[i] > array[i + 1]) {
-        return false;
+  public static void main(String[] args) {
+    int quantityOfValues;
+
+    while((quantityOfValues = scanner.nextInt()) != 0) {
+      boolean marceloTurn = true;
+      int sequence[] = new int[quantityOfValues];
+
+      for (int i = 0 ; i < quantityOfValues ; i++) {
+        sequence[i] = scanner.nextInt();
       }
+
+      int index = 0;
+      while (index != -1) {
+        index = findFirstUnsortedPair(sequence);
+
+        if(index != -1) {
+          switchNumbers(sequence, index, index + 1);
+          marceloTurn = !marceloTurn;
+        }
+      }
+
+      String winner = marceloTurn ? "Carlos" : "Marcelo";
+      System.out.println(winner);
     }
-    return true;
   }
 
-  public static void switchNumbers(Integer[] array, int index, int nextIndex) {
+  private static void switchNumbers(
+    int array[], int index, int nextIndex
+  ) {
     int aux = array[index];
     array[index] = array[nextIndex];
     array[nextIndex] = aux;
   }
 
-  public static int findFirstUnsortedPair(Integer[] array) {
+  private static int findFirstUnsortedPair(int array[]) {
     for (int i = 1; i < array.length; i++) {
       if (array[i] < array[i - 1]) {
         return i - 1;
       }
     }
     return -1;
-  }
-
-  public static void main(String[] args) {
-    boolean marceloTurn = true;
-
-    String values[] = sc.nextLine().split(" ");
-    Integer[] sequence = new Integer[values.length];
-
-    for (int i = 0; i < values.length; i++) {
-      sequence[i] = Integer.valueOf(values[i]);
-    }
-
-    while (!isArraySorted(sequence)) {
-      int index = findFirstUnsortedPair(sequence);
-
-      if(index != -1) {
-        switchNumbers(sequence, index, index + 1);
-        marceloTurn = !marceloTurn;
-      }
-    }
-
-    String winner = marceloTurn ? "Carlos" : "Marcelo";
-    System.out.println(winner);
   }
 }
