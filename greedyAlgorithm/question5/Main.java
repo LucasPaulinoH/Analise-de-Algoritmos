373b7f5260c5eb734489d6d9d93c00dccdc0334e
LucasPaulinoH <lucashsp12@hotmail.com>
2023-03-16 12:04:02 -0300

    fixed(question2): spacing adjustment

--- greedyAlgorithm/question5/Main.java
+++ greedyAlgorithm/question5/Main.java
@@ -24,6 +24,7 @@ public class Main {
         soldiersN = orderSoldiers(soldiersN);
         
         int battlesWin = 0;
+        
         for (int i = 0; i < quantitySoldiers; i++) {
             if(soldiersN[i] > soldiersQ[i]) {
                 battlesWin++;
