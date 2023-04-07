39c31d2142bff85b974daf53d92e5ea08c7e774d
RiadOliveira <riad.oliveira@hotmail.com>
2023-04-07 04:03:35 -0300

    DP question 4 final version and print beecrowd

--- dynamicProgramming/question4/Main.java
+++ dynamicProgramming/question4/Main.java
@@ -1,68 +1,33 @@
 package dynamicProgramming.question4;
 
-import java.util.Collection;
 import java.util.HashMap;
 import java.util.Scanner;
 
 public class Main {
     private static final Scanner scanner = new Scanner(System.in);
 
-    public static class PipeData {
-        public int length, value;
-
-        public PipeData(int length, int value) {
-            this.length = length;
-            this.value = value;
-        }
-
-        public String generateKey() {
-            StringBuilder stringBuilder = new StringBuilder();
-            return stringBuilder.append(length).append(value).toString();
-        }
-    }
-
     public static void main(String args[]) {
         final int quantityOfPipeSizes = scanner.nextInt();
         final int sizeOfProducedPipe = scanner.nextInt();
 
-        HashMap<String, PipeData> orderedPipes = new HashMap<>(quantityOfPipeSizes);
         int maxSolution = 0;
+        HashMap<Integer, Integer> previousSolutions = new HashMap<>();
 
         for(int ind=0 ; ind<quantityOfPipeSizes ; ind++) {
             int length = scanner.nextInt();
             int value = scanner.nextInt();
-            
-            PipeData pipe = new PipeData(length, value);
-            orderedPipes.put(pipe.generateKey(), pipe);
-            maxSolution = Math.max(value, maxSolution);
-        }
-        
-        HashMap<String, PipeData> previousSolutions = orderedPipes;
-        Collection<PipeData> orderedPipesValues = orderedPipes.values();
-        do {
-            HashMap<String, PipeData> currentSolutions = new HashMap<>(
-                previousSolutions.size()
-            );
-            Collection<PipeData> previousSolutionsValues = previousSolutions.values();
 
-            for(PipeData orderedPipe : orderedPipesValues) {
-                int pipeValue = orderedPipe.value;
-                int pipeLength = orderedPipe.length;
+            for(int nextLength = length; nextLength <= sizeOfProducedPipe; nextLength++) {
+                int remainingLengthSolution = previousSolutions.getOrDefault(nextLength - length, 0);
+                if(remainingLengthSolution == 0 && nextLength > length) continue;
 
-                for(PipeData previousSolution : previousSolutionsValues) {
-                    int solutionLength = previousSolution.length + pipeLength;
-                    if(solutionLength > sizeOfProducedPipe) continue;
-                    
-                    int solutionValue = previousSolution.value + pipeValue;
-                    PipeData pipe = new PipeData(solutionLength, solutionValue);
+                int newSolution = remainingLengthSolution + value;
+                int currentSolution = previousSolutions.getOrDefault(nextLength, 0);
 
-                    currentSolutions.put(pipe.generateKey(), pipe);
-                    maxSolution = Math.max(solutionValue, maxSolution);
-                }
+                previousSolutions.put(nextLength, Math.max(newSolution, currentSolution));
+                maxSolution = Math.max(maxSolution, newSolution);
             }
-
-            previousSolutions = currentSolutions;
-        } while(!previousSolutions.isEmpty());
+        }
 
         System.out.println(maxSolution);
     }
